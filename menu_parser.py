7650907fdede7fcef4af223f617fc5f39a2666db
Andrew T <16790483+ateesdalejr@users.noreply.github.com>
2018-05-23 15:36:44 -0700

    completed parse_dir

--- menu_parser.py
+++ menu_parser.py
@@ -1,37 +1,42 @@
-import string
-
-class MenuToken:
-    def __init__(self, type, value = None):
-        self.type = type
-        self.value = value
-
-    def __str__(self):
-        return f"MenuToken(type = {self.type}, value = {self.value})"
-
-    def __repr__(self):
-        return self.__str__()
+import sys
 
 class MenuParser:
     def __init__(self, text):
         self.text = text
         self.i, self.char = -1, None
         self.get_char()
-    
+
     def get_char(self):
         self.i += 1
         if self.i < len(self.text):
             self.char = self.text[self.i]
         else:
-            self.char = None
+            self.char = -1
+
+    def error(self, msg):
+        print(f"error: {msg}", file = sys.stderr) # put error here for match failure
+        sys.exit(-1)
+
+    def match(self, c):
+        buf = ""
+        while self.char != -1 and len(c) > len(buf):
+            buf += self.char
+            self.get_char()
+        if c != buf:
+            c, buf = repr(c), repr(buf)
+            self.error(f"expected {c} not {buf}")
 
     def grab_type(self):
         c = self.char
+        if not c in "0123456789+TgI":
+            c = repr(c)
+            self.error("unknown type {c}")
         self.get_char()
         return c
 
     def grab_unascii(self):
         unascii = ""
-        while not self.char in "\x00\t\r\n":
+        while self.char != -1 and not self.char in "\x00\t\r\n":
             unascii += self.char
             self.get_char()
         return unascii
@@ -39,7 +44,7 @@ class MenuParser:
     def grab_host(self):
         host = ""
         while True:
-            while not self.char in ".\x00\t\r\n":
+            while self.char != -1 and not self.char in ".\x00\t\r\n":
                 host += self.char
                 self.get_char()
             if self.char == ".":
@@ -48,24 +53,31 @@ class MenuParser:
             else:
                 break
         return host
-        
 
     def grab_port(self):
-        pass
-
-    def grab_last_line(self):
-        pass
+        port = ""
+        while self.char != -1 and self.char in "0123456789":
+            port += self.char
+            self.get_char()
+        if len(port) == 0:
+            self.error("port empty")
+        return int(port)
 
     def parse_dir(self):
         type_char = self.grab_type()
         user_name = self.grab_unascii()
-        self.get_char()
-        host_name = self.grab_host()
-        return type_char, user_name, host_name
+        self.match("\t")
+        selector = self.grab_unascii()
+        self.match("\t")
+        host = self.grab_host()
+        self.match("\t")
+        port = self.grab_port()
+        self.match("\r\n")
+        return type_char, user_name, host, port
 
 def run_tests():
-    prices = b"0About PricesFPrices/AboutusFpserver.bookstore.umn.eduF70\r\n0Macintosh PricesFPrices/MacFpserver.bookstore.umn.eduF70\r\n0IBM PricesFPrices/IckFpserver.bookstore.umn.eduF70\r\n0Printer & Peripheral PricesFPrices/PPPFpserver.bookstore.umn.eduF70\r\n"
-    p = MenuParser(b"0Example\tgopher.example.com\r\n".decode("ascii"))
-    print(p.parse_dir())
+    p = MenuParser(b"0\t\tgopher.example.com\t70\r\n".decode("ascii"))
+    print(p.parse_dir() == ("0", "", "gopher.example.com", 70))
+
 if __name__ == "__main__":
     run_tests()
